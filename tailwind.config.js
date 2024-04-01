b3bad3a011b7015698dff7ab08a1abdf26860b16
Kristo Samosir <kristoisthebest@gmail.com>
2024-04-01 21:58:01 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        'biru' : '#3282B8',
+        'biru-muda' : '#BBE1FA',
+        'biru-tua' : '#0F4C75',
+        'hitam' : '#1B262C'
+      },
+      fontFamily : {
+        'Concert-one' : ['Concert One']
+      }
+    },
+  },
+  plugins: [],
+}
+
