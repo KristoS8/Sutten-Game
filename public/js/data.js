cc4d39203f4e47936aa9a5ee8dc4644a8b807752
Kristo Samosir <kristoisthebest@gmail.com>
2024-04-10 15:41:41 +0900

    menambah logika pilihan user

--- public/js/data.js
+++ public/js/data.js
@@ -1,11 +1,25 @@
 let btnStartGame = document.getElementById('btnStartGame');
 let matchGame = document.getElementById('matchGame');
 
-matchGame.style.visibility = "hidden";
+let options = document.querySelectorAll('.option_img');
+
+const userCoice = document.querySelector('.userCoice img');
+const botCoice = document.getElementById('botCoice');
+const divBotCoice = document.getElementById('divBotCoice');
+
+let randomNumber = Math.floor(Math.random() * 3)
+
+options.forEach((image,index) => {
+  image.addEventListener("click", (e) => {
+    let imgSrc = e.target.src;
+    userCoice.src = imgSrc;
+  })
+})
+
+matchGame.style.visibility = 'hidden';
 
 btnStartGame.hidden = false;
 btnStartGame.addEventListener('click', function startGame() {
-
   btnStartGame.hidden = true;
-  matchGame.style.visibility = "visible";
+  matchGame.style.visibility = 'visible';
 });
