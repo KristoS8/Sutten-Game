51bb6c8f43ac4b585f4d50e8252d2cb370982b2d
Kristo Samosir <kristoisthebest@gmail.com>
2024-04-13 22:20:48 +0900

    tambah skor tertinggi,local storage,pilihan waktu,dan retry

--- public/js/data.js
+++ public/js/data.js
@@ -5,6 +5,14 @@ let options = document.querySelectorAll('.option_img');
 let timeToStart = document.getElementById('timeToStart');
 let matchTime = document.getElementById('matchTime');
 let divOptionUser = document.getElementById('divOptionUser');
+let gameRules = document.getElementById('aturanGame');
+let selectTime = document.getElementById('selectTime');
+let groupTime = document.getElementById('groupTime');
+let time10s = document.getElementById('time10s');
+let time30s = document.getElementById('time30s');
+let time1m = document.getElementById('time1m');
+let retry = document.getElementById('retry');
+let highestSkor = document.getElementById('highestSkor');
 
 const userCoice = document.querySelector('.userCoice img');
 const botCoice = document.querySelector('.botCoice img');
@@ -12,53 +20,84 @@ let result = document.getElementById('hasil');
 let skor = document.getElementById('skor');
 let skorMatch = 0;
 let detik = 3;
-let detikPertandingan = 29;
+let detikPertandingan = 0;
 
-options.forEach((image,index) => {
+groupTime.style.display = 'none';
+matchTime.style.display = 'none';
+retry.style.display = 'none';
+highestSkor.style.display = 'block';
+let LsSkor = localStorage.getItem('skorTertinggi');
+highestSkor.innerHTML = "Skor Tertinggi: " + LsSkor;
 
-    image.addEventListener("click", (e) => {
-      let imgSrc = e.target.src;
-      userCoice.src = imgSrc;
+if (LsSkor == null) {
+  highestSkor.style.display = 'none';
+}
 
-      let randomNumber = Math.floor(Math.random() * 3);
+selectTime.addEventListener('click', function () {
+  selectTime.style.display = 'none';
+  groupTime.style.display = 'flex';
 
-      let imgAsset = ["img/littleFinger.png", "img/indexFinger.png" ,"img/thumbsUp.png"]
-    
-      botCoice.src = imgAsset[randomNumber];
-    
-      let userValue = ["LF", "IF", "T"][index];
-      let botValue = ["LF", "IF", "T"][randomNumber];
+  time10s.addEventListener('click', function () {
+    time30s.style.display = 'none';
+    time1m.style.display = 'none';
+    detikPertandingan = 10;
+  });
 
-      let hasil = {
-        LFLF : "Seri",
-        LFIF : "Bot Menang, Kamu Kalah Point-1",
-        LFT : "Kamu Menang, Bot Kalah Point+3",
-        IFLF : "Kamu Menang, Bot Kalah Point+3",
-        IFIF : "Seri",
-        IFT : "Bot Menang, Kamu Kalah Point-1",
-        TLF : "Bot Menang, Kamu Kalah Point-1",
-        TIF : "Kamu Menang, Bot Kalah Point+3",
-        TT : "Seri",
-      };
+  time30s.addEventListener('click', function () {
+    time10s.style.display = 'none';
+    time1m.style.display = 'none';
+    detikPertandingan = 30;
+  });
 
-      let hasilMatch = hasil[userValue + botValue];
- 
-      if (userValue == botValue) {
-        result.innerHTML = "Seri , 0 Point"
-        skorMatch = skorMatch + 0;
-      }else{
-        result.innerHTML = hasilMatch
-        if (hasilMatch == "Kamu Menang, Bot Kalah Point+3") {
-          skorMatch = skorMatch + 3
-        }else if (hasilMatch == "Bot Menang, Kamu Kalah Point-1") {
-          skorMatch = skorMatch - 1
-        }
+  time1m.addEventListener('click', function () {
+    time30s.style.display = 'none';
+    time10s.style.display = 'none';
+    detikPertandingan = 60;
+  });
+});
+
+options.forEach((image, index) => {
+  image.addEventListener('click', (e) => {
+    let imgSrc = e.target.src;
+    userCoice.src = imgSrc;
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    let imgAsset = ['img/littleFinger.png', 'img/indexFinger.png', 'img/thumbsUp.png'];
+
+    botCoice.src = imgAsset[randomNumber];
+
+    let userValue = ['LF', 'IF', 'T'][index];
+    let botValue = ['LF', 'IF', 'T'][randomNumber];
+
+    let hasil = {
+      LFLF: 'Seri',
+      LFIF: 'Bot Menang, Kamu Kalah Point-1',
+      LFT: 'Kamu Menang, Bot Kalah Point+3',
+      IFLF: 'Kamu Menang, Bot Kalah Point+3',
+      IFIF: 'Seri',
+      IFT: 'Bot Menang, Kamu Kalah Point-1',
+      TLF: 'Bot Menang, Kamu Kalah Point-1',
+      TIF: 'Kamu Menang, Bot Kalah Point+3',
+      TT: 'Seri',
+    };
+
+    let hasilMatch = hasil[userValue + botValue];
+
+    if (userValue == botValue) {
+      result.innerHTML = 'Seri , 0 Point';
+      skorMatch = skorMatch + 0;
+    } else {
+      result.innerHTML = hasilMatch;
+      if (hasilMatch == 'Kamu Menang, Bot Kalah Point+3') {
+        skorMatch = skorMatch + 3;
+      } else if (hasilMatch == 'Bot Menang, Kamu Kalah Point-1') {
+        skorMatch = skorMatch - 1;
       }
+    }
 
-      skor.textContent = "Skor Kamu : " + skorMatch
-
-    });
-
+    skor.textContent = 'Skor Kamu : ' + skorMatch;
+  });
 });
 
 matchGame.style.visibility = 'hidden';
@@ -67,39 +106,51 @@ timeToStart.style.visibility = 'hidden';
 
 btnStartGame.hidden = false;
 btnStartGame.addEventListener('click', function startGame() {
-
-  timeToStart.style.visibility = 'visible'
+  highestSkor.style.display = 'none';
+  gameRules.parentNode.removeChild(gameRules);
+  timeToStart.style.visibility = 'visible';
   let Interval = setInterval(() => {
     timeToStart.innerHTML = detik;
     detik--;
+    
 
     if (detik === -1) {
       timeToStart.innerHTML = "Let's Play!";
+      time10s.style.display = 'none';
+      time30s.style.display = 'none';
+      time1m.style.display = 'none';
+      selectTime.style.display = 'none';
     }
 
     if (detik < -1) {
       clearInterval(Interval);
-      timeToStart.style.visibility = 'hidden';
+      timeToStart.parentNode.removeChild(timeToStart);
       matchGame.style.visibility = 'visible';
       hasil.style.visibility = 'visible';
+      matchTime.style.display = 'block';
 
       let waktuPertandingan = setInterval(() => {
-        matchTime.innerHTML = detikPertandingan + "s"
+        matchTime.innerHTML = detikPertandingan + 's';
         detikPertandingan--;
-      
-        if (detikPertandingan < -1) {
-          matchTime.innerHTML = "waktu habis!"
-          divOptionUser.style.pointerEvents = 'none';
-          result.style.visibility = 'hidden';
-        }
-      
-      }, 1000);
 
+        if (detikPertandingan < -1) {
+          clearInterval(waktuPertandingan);
+          matchTime.innerHTML = 'waktu habis!';
+          divOptionUser.style.pointerEvents = 'none';
+          result.style.display = 'none';
+          skor.innerHTML = 'Match Selesai, Skor Kamu: ' + skorMatch;
+          retry.style.display = 'block';
+
+          localStorage.setItem('skorTertinggi', skorMatch);
+        }
+      }, 1000);
     }
   }, 1000);
 
   btnStartGame.hidden = true;
-
-  
-
 });
+
+retry.addEventListener('click', function () {
+  retry = window.location.reload();
+});
+
